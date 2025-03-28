c06cb2aa085406c75b3f1157c08f12336d244a7b
Hanzeel Villa <hanzeelvilla0@gmail.com>
2025-03-27 22:24:03 -0600

    Diseño basico

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <h1>Walle To Do List</h1>
+      <input type="text" />
+      <button>+</button>
+      <p>Tarea 1</p>
+    </>
+  )
+}
+
+export default App
