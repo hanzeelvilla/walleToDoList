da597ef48b504988caea138f721c9bdfd5334fad
Hanzeel Villa <hanzeelvilla0@gmail.com>
2025-03-29 22:17:45 -0600

    first design

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
-import "./App.css"
+import "./App.css";
+import addIcon from "./assets/add.png"
+import penIcon from "./assets/pen.png"
+import deleteIcon from "./assets/delete.png"
 
 function App() {
   return (
     <>
       <div className="main-container">
-        <h1>Walle To Do List</h1>
-        <div>
-          <input type="text" />
-          <button>+</button>
+        <h1>Walle To-Do List</h1>
+        <div className="input-main">
+        <label htmlFor="todo">Add a To-Do</label>
+        <div className="input-container">
+          <input type="text" name="todo"/>
+          <img src={addIcon} alt="Add icon" className="icon"/>
         </div>
-        <p>Tarea 1</p>
+        </div>
+        <div className="todo">
+          <p>Sacar a pasear al viejon</p>
+          <div>
+            <img src={penIcon} alt="Pen icon" className="icon"/>
+            <img src={deleteIcon} alt="Delte icon" className="icon"/>
+          </div>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
